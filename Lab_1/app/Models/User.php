ffd0cddf3590f10288006cf5cdf8c4c5e9f2772e
EkaterinaUmetskaya <umetskaya_ekaterina@mail.ru>
2026-05-23 13:48:39 +0300

    Лаба2

--- Lab_1/app/Models/User.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Models;
-
-use TCG\Voyager\Models\User as VoyagerUser;
-
-class User extends VoyagerUser
-{
-}
--- Lab_1/routes/api.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UploadController;
-use App\Http\Controllers\StudyLoadController;
